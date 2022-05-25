380a75ce8c90b541dc4e8d34c35c1bc55ed6cb58
phuong-ta <phuong-ta@users.noreply.github.com>
2022-05-25 12:56:13 +0300

    edited excercise1

--- 1. Min Max/MinMax.c
+++ 1. Min Max/MinMax.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 
 int checkNumber(int number1, int number2) {
   if (number1>number2)
   {
-    char result = printf("big number is %d", number1);
-    return result;
+    return number1;
   }else{
-    char result = printf("big number is %d", number2);
-    return result;
+    return number2;
   }
-  
-  
 }
 
 int main(void) {
   int firstNumber;
   int secondNumber;
-  scanf(" First number is %d", &firstNumber);
   scanf(" First number is %d", &secondNumber);
+  scanf(" First number is %d", &firstNumber);
   int bigNumber = checkNumber(firstNumber, secondNumber);
   printf("big number is %d", bigNumber);
 }
